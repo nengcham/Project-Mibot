36cbbab917198b147f040cbaf87958194fcf0a30
최건일 <97512602+nengcham@users.noreply.github.com>
2022-08-16 13:58:29 +0900

    Update settings.py

--- mibot-fastapi/admin/settings.py
+++ mibot-fastapi/admin/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--1k%8hnlu4zx-kw04uyo@$@i6hysfire4e(#vh_7yf)6mug^^*'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
